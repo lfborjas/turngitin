7ae5bd12a489f4cbfe9ae36a505fb528b0bb8fe4
lfborjas <luis.borjas@escolarea.com>
2010-11-18 18:04:54 -0600

    creo que está listo para deploy

--- admin.rb
+++ admin.rb
@@ -1,5 +1,6 @@
-require 'sinatra/base'
+require 'sinatra'
 require 'dm-core'
+require 'dm-migrations'
 require 'haml'
 require 'json'
 
@@ -9,23 +10,19 @@ class PushInfo
     property :message, Text
 end
 
-class Admin < Sinatra::Base
-    set :views, File.dirname(__FILE__) + '/admin_views'    
-    set :public, File.dirname(__FILE__) + '/public'
-    configure do 
-         DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/development.sqlite3"))
-         DataMapper.auto_upgrade!
-    end
+configure do 
+     DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/development.sqlite3"))
+     DataMapper.auto_upgrade!
+end
 
-    get '/' do 
-        @raw_messages = PushInfo.all
-        @messages = @raw_messages.collect {|m| JSON.parse(m.message)}
-        #p @messages
-        haml :dashboard, :layout=>false
-    end
-    
-    post '/turnin' do 
-        PushInfo.create :message => params[:payload]
-        "OK"
-    end
+get '/' do 
+    @raw_messages = PushInfo.all
+    @messages = @raw_messages.collect {|m| JSON.parse(m.message)}
+    #p @messages
+    haml :dashboard, :layout=>false
+end
+
+post '/turnin' do 
+    PushInfo.create :message => params[:payload]
+    "OK"
 end
--- config.ru
+++ config.ru
@@ -1,17 +1,3 @@
-require 'slackernews'
-require 'exam'
 require 'admin'
 
-map "/" do 
-    run SlackerNews 
-end
-
-map "/examen" do 
-    run Exam 
-end
-
-map "/admin" do 
-    run Admin
-end
-
-
+run Sinatra::Application
