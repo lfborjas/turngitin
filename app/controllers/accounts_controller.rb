31ea6d59cbb7687bb8df77eb4a0b802dc94024f2
lfborjas <luis.borjas@escolarea.com>
2010-11-20 00:50:23 -0600

    added generated files, FFUUU

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,20 @@
+class AccountsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def github
+  end
+
+end
